b4c9f1351ae5110453facf88b7e03af4fd0fda84
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-04-25 15:31:21 +0100

    added javascript code to add typing animation.

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function typeText() {
-    var mainText = "Sign up and be the first to experience personalized tailoring at it’s finest!"; // Text to be typed
+    var mainText = "Sign up and be the first to experience personalized tailoring at it’s finest!";
     var interval = 100; // Adjust the typing speed (in milliseconds)
     var index = 0; // Initialize the index for typing
-
+    
     var paragraphElement = document.querySelector('.content p');
-
+    
     // Clear existing text content
     paragraphElement.textContent = '';
-
+    
     function typeNextLetter() {
         // Check if all text is typed
         if (index === mainText.length) {
             return;
         }
-
+        
         // Concatenate next letter to the current text
         var newText = mainText.substring(0, index + 1);
         paragraphElement.textContent = newText;
-
+        
         // Increment index for next letter
         index++;
-
+        
         // Set timeout to call itself recursively
         setTimeout(typeNextLetter, interval);
     }
-
+    
     // Start typing text
     setTimeout(typeNextLetter, 2000); // Delay before starting the typing animation
 }
